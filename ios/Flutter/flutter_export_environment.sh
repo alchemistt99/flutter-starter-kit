1dd01ea7f49ad556ed1677911cc8c71a06229b10
Aldi Rohman <aldirohman@live.com>
2020-01-25 09:36:52 +0700

    change component to widgets & update dependencies

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/aldi/Tools/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/aldi/Playground/mobile/flutter/flutter_starter_kit"
+export "FLUTTER_ROOT=/home/aldi/engine/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/aldi/Playground/mobile/flutter/flutter-starter-kit"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/aldi/Tools/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/aldi/engine/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
--- lib/screens/login/loginForm.dart
+++ lib/screens/login/loginForm.dart
@@ -2,8 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_starter_kit/blocs/login/bloc.dart';
-import 'package:flutter_starter_kit/components/DialogTextOnly.dart';
 import 'package:flutter_starter_kit/constants/Dictionary.dart';
+import 'package:flutter_starter_kit/widgets/DialogTextOnly.dart';
 
 class LoginForm extends StatefulWidget {
   @override
